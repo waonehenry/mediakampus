c97a1f86179296d6ac2ef51cf103a453660a61ea
Hendri <hendri.kurniawan.p@gmail.com>
2019-05-07 13:44:02 +0700

    login view

---
+++ application/modules/login/models/Login_model.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login_model extends CI_Model {
+		public function resolve_user_login($username, $password) {
+
+				$this->db->select('password');
+				$this->db->from('users');
+				$this->db->where('username', $username);
+				$hash = $this->db->get()->row('password');
+
+				return $this->verify_password_hash($password, $hash);
+
+		}
+
+		public function get_user_id_from_username($username) {
+
+				$this->db->select('id');
+				$this->db->from('users');
+				$this->db->where('username', $username);
+
+				return $this->db->get()->row('id');
+
+		}
+
+		public function get_user($user_id) {
+
+				$this->db->from('users');
+				$this->db->where('id', $user_id);
+				return $this->db->get()->row();
+
+		}
+
+		private function verify_password_hash($password, $hash) {
+
+				return password_verify($password, $hash);
+
+		}
+}
