821e5deeff97dffb8e071d41e67783ae54a4ecfd
Hendri <hendri.kurniawan.p@gmail.com>
2019-10-11 10:33:53 +0700

    lali opo sing diedit

--- application/modules/home/views/layout/foot.php
+++ application/modules/home/views/layout/foot.php
@@ -42,7 +42,8 @@ function formFocus() {
 $(document).ready(function() {
     formFocus();
     $('.modal').modal();
-    $(".input-date").inputmask("date", {alias:"dd-mm-yyyy"});
+    // $(".input-date").inputmask("date", {alias:"dd-mm-yyyy"});
+    $(".input-date").datetimepicker();
     $(".select2").select2({
         allowClear: true
     });
